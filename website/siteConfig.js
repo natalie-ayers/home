c579b8a93d324d6dcae42f57a338f6f96dc4ea92
natalie-ayers <46323737+natalie-ayers@users.noreply.github.com>
2022-07-25 13:17:05 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const siteConfig = {
     tagline: 'Curious, enthusiastic technologist using data science and tech for conflict and security applications',
     profilePhoto: 'img/profile.png',
     url: 'https://natalie-ayers.github.io', // Your website URL
-    baseUrl: '/tech-portfolio/', // Base URL for your project */
+    baseUrl: '/home/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
